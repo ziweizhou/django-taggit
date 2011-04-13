8323ebf6d08def1a40a44b1c134cd6b862713f53
Josh West <joshwest@atlanticmediacompany.com>
2011-04-13 15:52:36 -0400

    Revert "Can no longer create two different tags with different capitalizations."
    This reverts commit 355e1a6f74c33b469c68ebf5e0fba6f3fe8ea6fd.

--- taggit/managers.py
+++ taggit/managers.py
@@ -6,8 +6,6 @@ from django.db.models.related import RelatedObject
 from django.db.models.fields.related import add_lazy_relation
 from django.utils.translation import ugettext_lazy as _
 
-from django.template.defaultfilters import slugify
-
 from taggit.forms import TagField
 from taggit.models import TaggedItem, GenericTaggedItemBase
 from taggit.utils import require_instance_manager
@@ -161,14 +159,10 @@ class _TaggableManager(models.Manager):
         existing = self.through.tag_model().objects.filter(
             name__in=str_tags
         )
-
         tag_objs.update(existing)
 
-        existing_slugs = set([t.slug for t in tag_objs])
-		
-        for new_tag in str_tags:
-            if slugify(new_tag) not in existing_slugs:
-            	tag_objs.add(self.through.tag_model().objects.create(name=new_tag))
+        for new_tag in str_tags - set(t.name for t in existing):
+            tag_objs.add(self.through.tag_model().objects.create(name=new_tag))
 
         for tag in tag_objs:
             self.through.objects.get_or_create(tag=tag, **self._lookup_kwargs())
