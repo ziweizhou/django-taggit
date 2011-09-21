1c0fc2a4b302927ce5041d374df225d6d836ed0a
Henrik Almer <henrik@agoodid.se>
2011-09-21 15:31:19 +0200

    Added a count of tagged items to the admin list view

--- taggit/admin.py
+++ taggit/admin.py
@@ -3,11 +3,18 @@ from django.contrib import admin
 from taggit.models import Tag, TaggedItem
 
 
+def tagged_items_count(obj):
+    tagged_items_count = TaggedItem.objects.filter(tag=obj).count()
+    return tagged_items_count
+tagged_items_count.short_description = 'Tagged Items Count'
+
+
 class TaggedItemInline(admin.StackedInline):
     model = TaggedItem
 
+
 class TagAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+    list_display = ["name", tagged_items_count]
     inlines = [
         TaggedItemInline
     ]
