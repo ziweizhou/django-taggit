87c13fbfebf219ef6a0a83c0f612787fe5cca76d
maccesch <maccesch@web.de>
2012-01-12 18:18:38 +0100

    added tag name admin filter

--- taggit/adminfilters.py
+++ taggit/adminfilters.py
@@ -1,5 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.admin import SimpleListFilter
+from models import TagBase
 
 class TagNameListFilter(SimpleListFilter):
     # Human-readable title which will be displayed in the
@@ -7,7 +8,7 @@ class TagNameListFilter(SimpleListFilter):
     title = _('by tag')
 
     # Parameter for the filter that will be used in the URL query.
-    parameter_name = 'decade'
+    parameter_name = 'tag'
 
     def lookups(self, request, model_admin):
         """
@@ -17,10 +18,14 @@ class TagNameListFilter(SimpleListFilter):
         human-readable name for the option that will appear
         in the right sidebar.
         """
-        return (
-            ('80s', _('in the eighties')),
-            ('90s', _('in the nineties')),
-        )
+        
+        tags = TagBase.objects.all()
+        
+        options = []
+        for tag in tags:
+            options.append((tag.slug, tag.name));
+        
+        return options
 
     def queryset(self, request, queryset):
         """
@@ -28,11 +33,4 @@ class TagNameListFilter(SimpleListFilter):
         provided in the query string and retrievable via
         `self.value()`.
         """
-        # Compare the requested value (either '80s' or 'other')
-        # to decide how to filter the queryset.
-        if self.value() == '80s':
-            return queryset.filter(birthday__year__gte=1980,
-                                    birthday__year__lte=1989)
-        if self.value() == '90s':
-            return queryset.filter(birthday__year__gte=1990,
-                                   birthday__year__lte=1999)
+        return queryset.filter(tags__slug=self.value())
