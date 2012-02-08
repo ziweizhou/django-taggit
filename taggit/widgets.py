419c8c20fb8b394ba5bb86010b544671d00e0dc3
Henrik Almer <henrik@agoodid.se>
2012-02-08 11:09:51 +0100

    More i18n and l10n

--- taggit/widgets.py
+++ taggit/widgets.py
@@ -14,7 +14,7 @@ class TagAutocomplete(forms.TextInput):
         if value is not None and not isinstance(value, basestring):
             value = edit_string_for_tags([o.tag for o in value.select_related("tag")])
         html = super(TagAutocomplete, self).render(name+"_dummy", u'', attrs)
-        js = u'<script type="text/javascript">jQuery = django.jQuery; jQuery().ready(function() { jQuery("#%s").autoSuggest("%s", {startText: "Enter Tag Here", preFill: "%s"}); });</script>' % (attrs['id'], list_view, escapejs(value) if value else u'')
+        js = u'<script type="text/javascript">jQuery = django.jQuery; jQuery().ready(function() { jQuery("#%s").autoSuggest("%s", {startText: "%s", preFill: "%s"}); });</script>' % (attrs['id'], list_view, _("Enter Tag Here"), escapejs(value) if value else u'')
         return mark_safe("\n".join([html, js]))
     
     class Media:
