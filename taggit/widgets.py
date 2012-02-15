6d96c7822fd37be10dc79a38e9b7051ed8bc7578
Henrik Almer <henrik@agoodid.se>
2012-02-15 10:41:24 +0100

    Added line breaks for better PEP8 compliance

--- taggit/widgets.py
+++ taggit/widgets.py
@@ -13,13 +13,26 @@ class TagAutocomplete(forms.TextInput):
     def render(self, name, value, attrs=None):
         list_view = reverse('taggit-list')
         if value is not None and not isinstance(value, basestring):
-            value = edit_string_for_tags([o.tag for o in value.select_related("tag")])
-        html = super(TagAutocomplete, self).render(name+"_dummy", u'', attrs)
-        js = u'<script type="text/javascript">jQuery = django.jQuery; jQuery().ready(function() { jQuery("#%s").autoSuggest("%s", {startText: "%s", preFill: "%s"}); });</script>' % (attrs['id'], list_view, _("Enter Tag Here"), escapejs(value) if value else u'')
+            value = edit_string_for_tags(
+              [o.tag for o in value.select_related("tag")]
+            )
+        html = super(TagAutocomplete, self).render(
+            name+"_dummy",
+            u'',
+            attrs
+        )
+        js = u'<script type="text/javascript">jQuery = django.jQuery; '
+            'jQuery().ready(function() { jQuery("#%s").autoSuggest("%s", '
+            '{startText: "%s", preFill: "%s"}); });</script>'
+            % (attrs['id'], list_view, _("Enter Tag Here"),
+                escapejs(value) if value else u'')
         return mark_safe("\n".join([html, js]))
     
     class Media:
-        js_base_url = getattr(settings, 'TAGGIT_AUTOCOMPLETE_JS_BASE_URL','%sjquery-autocomplete' % settings.STATIC_URL)
+        js_base_url = getattr(
+            settings,
+            'TAGGIT_AUTOCOMPLETE_JS_BASE_URL',
+            '%sjquery-autocomplete' % settings.STATIC_URL)
         css = {
             'all': ('%s/css/autoSuggest.css' % js_base_url,)
         }
