3a0097f321ed01e4a570580bfa8023f283f1cf17
Anton Parkhomenko <chuwy18@gmail.com>
2012-12-08 22:19:26 +0800

    tagged_object_list rewritten on CBV; views/urls refactoring

--- taggit/urls.py
+++ taggit/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 
-from taggit import views
 
-
-urlpatterns = patterns('taggit.views',
+urlpatterns = patterns(
+    'taggit.views',
     url(r'^list$', 'list_tags', name='taggit-list'),
-    url(r'^static/(?P<path>.*)$', views.media, name='taggit-static'),
-    url(r'^ajax$', views.ajax, name='taggit-ajax'),
+    url(r'^static/(?P<path>.*)$', 'media', name='taggit-static'),
+    url(r'^ajax$', 'ajax', name='taggit-ajax'),
 )
